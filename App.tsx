6d68dacc7a704ba8b208c880e080dc348c07bd22
Paul Breinig <Breinigpaul@web.de>
2023-08-27 14:25:53 +0200

    make the android navbar and statusbar use the theme colors

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
+import { useEffect } from 'react';
+import { StatusBar } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { BottomTabNavigator } from './src/navigation/bottom-tab-navigator';
 import { SettingsProvider, useSettingsContext } from './src/context/settings-context';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 
 const AppContent = () => {
 	const { theme } = useSettingsContext();
 
+	useEffect(() => {
+		changeNavigationBarColor(theme.colors.surface, false, false);
+	}, [theme]);
+
 	return (
 		<PaperProvider theme={theme}>
+			<StatusBar backgroundColor={theme.colors.background} />
 			<NavigationContainer>
 				<BottomTabNavigator />
 			</NavigationContainer>
