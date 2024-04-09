3371cd6b7bff6a92d2c41d882e6772955dacef51
Paul Breinig <Breinigpaul@web.de>
2024-04-09 11:20:05 +0200

    add usePrevious hook

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { formatCurrency } from '@coingecko/cryptoformat';
 import { format } from 'date-fns';
 import { parse } from 'node-html-parser';
@@ -12,3 +13,11 @@ export const formatTimestamp = (timestamp: number, frmt?: string) =>
 	format(timestamp, frmt || 'EEE, MMM d HH:mm');
 
 export const stripHTML = (textWithHTML: string) => parse(textWithHTML).text;
+
+export const usePrevious = <T>(value: T): T | undefined => {
+	const ref = useRef<T>();
+	useEffect(() => {
+		ref.current = value;
+	});
+	return ref.current;
+};
